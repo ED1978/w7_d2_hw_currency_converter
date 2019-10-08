4c1a438b32f0a29f19fa8bf6a705078b1e443321
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-08 17:20:44 +0100

    currencies fetched and mounted

---
+++ app/src/app.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  new Vue({
+    el: "#app",
+
+    data: {
+      currencies: []
+    },
+
+    mounted(){
+      this.fetchCurrencies();
+    },
+
+    methods: {
+      fetchCurrencies: function(){
+        const request = fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => this.currencies = data);
+      }
+    }
+
+  });
+
+});
