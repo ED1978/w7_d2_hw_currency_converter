e5f4d05ab064395908a1a270b46c875d946bbf29
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-08 20:57:24 +0100

    first extension kind of

--- app/src/app.js
+++ app/src/app.js
@@ -8,9 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       exchangeRates: [],
       amount: 0,
-      rate: 0,
+      firstCurrency: 0,
+      secondCurrency: 0,
       fromEuroConversion: 0,
-      toEuroConversion: 0,
+      toEuroConversion: 0
     },
 
     mounted(){
@@ -24,12 +25,16 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(data => this.exchangeRates = data);
       },
       convertCurrencyFromEuros: function(){
-        const conversion = this.amount * this.rate;
+        const conversion = this.toEuroConversion * this.secondCurrency;
         this.fromEuroConversion = conversion;
       },
       convertCurrencyToEuros: function(){
-        const conversion = this.amount / this.rate;
+        const conversion = this.amount / this.firstCurrency;
         this.toEuroConversion = conversion;
+      },
+      convertNonBaseCurrencies: function(){
+        this.convertCurrencyToEuros();
+        this.convertCurrencyFromEuros();
       }
     }
 
